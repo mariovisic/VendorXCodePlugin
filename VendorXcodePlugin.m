336d1c8a43a93a1d19af1a9fccc571f2d6065edc
Mario Visic <mario@mariovisic.com>
2011-09-07 19:29:57 +0800

    Some work in having commands run on the console, the website launches correctly from the menu.

--- VendorXcodePlugin.m
+++ VendorXcodePlugin.m
@@ -35,7 +35,8 @@
     NSMenuItem *vendorMenuItem = [NSMenuItem new];
     NSMenu *vendorMenu = [[NSMenu alloc]initWithTitle:@"Vendor"];
 
-    [vendorMenu addItemWithTitle:@"Vendor Install" action:nil keyEquivalent:@""];
+    
+    [[vendorMenu addItemWithTitle:@"Vendor Install" action:@selector(vendorInstall:) keyEquivalent:@"d"] setTarget:self];
     [vendorMenu addItemWithTitle:@"Vendor Update" action:nil keyEquivalent:@""];
 
     [vendorMenu addItem:[NSMenuItem separatorItem]];
@@ -44,13 +45,73 @@
     
     [vendorMenu addItem:[NSMenuItem separatorItem]];
     
-    [vendorMenu addItemWithTitle:@"Find Vendor Packages" action:nil keyEquivalent:@""];
+    [[vendorMenu addItemWithTitle:@"Find Vendor Packages" action:@selector(findVendorPackages:) keyEquivalent:@""] setTarget:self];
     
     // Attach the menus created to the main menu.
     // Insert the Vendor menu 7th down the line (usually just before window).
     [vendorMenuItem setSubmenu:vendorMenu];
     [mainMenu insertItem:vendorMenuItem atIndex:7]; 
+    
+
  
+}
+
++ (void)findVendorPackages: (id) sender {
+    
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://vendorkit.com/"]];
+    
+}
+
+
++ (NSString*)getVendorPath {
+    
+    NSTask *task = [[NSTask new] autorelease];
+    NSPipe *pipe = [NSPipe pipe];
+    NSData *standardOutput = nil;
+
+    [task setLaunchPath:@"/bin/bash"];
+    [task setStandardOutput:pipe];    
+    [task setArguments: [NSArray arrayWithObjects:@"-l",
+    				 @"-c",
+    				 @"which vendor",
+    				 nil]];
+    [task launch];
+    [task waitUntilExit];
+    
+    if ([task terminationStatus] == 0) {
+        NSLog(@"Vendor path detected successfully");
+        standardOutput = [[pipe fileHandleForReading] readDataToEndOfFile];
+        return [[NSString alloc] initWithData:standardOutput encoding:NSUTF8StringEncoding];
+    } else {
+        NSLog(@"Had an error, oh noes");
+        return nil;
+    }
+    
+}
+
++ (void)vendorInstall: (id)sender {
+
+    NSString *vendorPath = [self getVendorPath];
+    NSTask *task = [NSTask new];    
+    
+    vendorPath = [self getVendorPath];
+    
+    NSLog(@"vendor path: %@", vendorPath);
+    
+    if(vendorPath != nil) {
+        
+        [task setLaunchPath:vendorPath];
+        [task setArguments:[NSArray arrayWithObject:@"install"]];
+        [task launch];
+        [task waitUntilExit];
+         
+         if ([task terminationStatus] == 0) {
+             NSLog(@"Vendor Install OK");
+         } else {
+             NSLog(@"Had an error, oh noes");
+         }
+    }
+         
 }
 
 @end
